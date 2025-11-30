bd1b59af9d1bdce97023b52fec168abcb6d6c069
= <=>
2025-11-30 11:08:12 -0500

    Enable WhiteNoise for static files

--- pr_eventsoft/settings.py
+++ pr_eventsoft/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'app_eventos.middleware.ActualizarEventosFinalizadosMiddleware',
